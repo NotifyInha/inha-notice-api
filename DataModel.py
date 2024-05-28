9364d540a07534144357b273366ab3ba48906a7b
Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
2024-05-28 16:13:36 +0900

    CRUD 모두 구현

--- DataModel.py
+++ DataModel.py
@@ -1,38 +1,43 @@
 from datetime import datetime
+from typing import Any
 import pytz
 from pydantic import BaseModel
 
+local_timezone = pytz.timezone('Asia/Seoul')
+
+class NoticeLight(BaseModel):
+    title:str
+    url:str
+    category:str
+    source:str
+    published_date:str
+    id:str
+    _id:Any
+
+    def __init__(self, title, url, category, source, published_date, _id = None):
+        super().__init__(title=title, url=url, category=category, source=source, published_date=published_date, _id=_id, id=str(_id)) 
 class Notice(BaseModel):
     title:str
-    content:str
-    image:list
-    attached:list
     url:str
     category:str
     source:str
     published_date:str
     scraped_date:str
+    id:str
+    _id:Any
+    content:str
+    image:list
+    attached:list
     is_sent_notification:bool
 
-class Notice_B():
-
-    
-    local_timezone = pytz.timezone('Asia/Seoul')
-    def __init__(self, title, content, image, attached, url, category, source, published_date, scraped_date = None, is_sent_notification = False, id = None):
-        self.title = title
-        self.content = content
-        self.image = image
-        self.attached = attached
-        self.url = url
-        self.category = category
-        self.source = source
-        self.published_date = published_date
+    def __init__(self, title, content, images, attached, url, category, source, published_date, scraped_date = None, is_sent_notification = False, _id = None):
         if scraped_date is None:
-            self.scraped_date = datetime.now().astimezone(Notice_B.local_timezone).isoformat()
-        else:
-            self.scraped_date = scraped_date
-        self.is_sent_notification = is_sent_notification
-        self.id = id
+            scraped_date = datetime.now().astimezone(local_timezone).isoformat()
+        if images is None:
+            images = []
+        if attached is None:
+            attached = []
+        super().__init__(title=title, content=content, image=images, attached=attached, url=url, category=category, source=source, published_date=published_date, scraped_date=scraped_date, is_sent_notification=is_sent_notification, _id=_id, id=str(_id))
 
     def to_dict(self):
         return {
@@ -47,6 +52,8 @@ class Notice_B():
             "scraped_date": self.scraped_date,
             "is_sent_notification": self.is_sent_notification
         }
+    def to_dict_with_fields(self, *args):
+        return {arg: self.__dict__[arg] for arg in args if arg in self.__dict__}
     @classmethod
     def from_dict(cls, data: dict):
         if "_id" in data:#_id가 있으면 id로 저장
@@ -64,14 +71,22 @@ class Notice_B():
         scraped_date = data["scraped_date"]
         is_sent_notification = data["is_sent_notification"]
         return cls(title, content, image, attached, url, category, source, published_date, 
-                   scraped_date = scraped_date, is_sent_notification = is_sent_notification, id = id)
+                   scraped_date = scraped_date, is_sent_notification = is_sent_notification, _id = id)
 
 
     def __str__(self):
         return f"title: {self.title}, content: {self.content}, image: {self.image}, attached: {self.attached}, url: {self.url}, category: {self.category}, source: {self.source}, published_date: {self.published_date}, scraped_date: {self.scraped_date}, is_sent_notification: {self.is_sent_notification}"
     
 
+
+class Page(BaseModel):
+    total_count:int
+    page:int
+    page_size:int
+    total_page:int
+    notices:list[Notice]
+
 if __name__ == "__main__":
-    notice = Notice_B("title", "content", [],[],"url", "category", "source", "published_date")
+    notice = Notice("title", "content", [],[],"url", "category", "source", "published_date")
     print(notice.to_dict())
     print(notice)
--- MongoDBWrapper.py
+++ MongoDBWrapper.py
@@ -1,7 +1,10 @@
+from bson import ObjectId
 import motor.motor_asyncio
 from pymongo.server_api import ServerApi
-from DataModel import Notice_B
+from DataModel import Notice
 from Config import connection_string
+from fastapi_pagination.ext.motor import paginate
+
 
 class MongoDBWrapper:
     def __init__(self, client) -> None:
@@ -17,34 +20,42 @@ class MongoDBWrapper:
             return cls(client)
         except Exception as e:
             print(type(e), e)
-       
-    def insert(self, data :Notice_B):
+
+    async def _check_duplicate(self, data :Notice):
+        db = self.client["inha_notice"]
+        collection = db["notice"]
+        return await collection.find_one({"url": data.url}) is not None
+
+    async def insert(self, data :Notice):
             # Get the database
             db = self.client["inha_notice"]
             # Get the collection
             collection = db["notice"]
             # Insert the data
-            if not self._check_duplicate(data):
-                collection.insert_one(data.to_dict())
+            if await self.need_update(data) == None:
+                await collection.insert_one(data.to_dict())
+                return True
+            return False
+                
         
-    def need_update(self, data :Notice_B):
+    async def need_update(self, data :Notice):
         db = self.client["inha_notice"]
         collection = db["notice"]
-        e = collection.find_one({"url": data.url}) 
+        e = await collection.find_one({"url": data.url}) 
         if e is not None:
-            item = Notice_B.from_dict(e)
+            item = Notice.from_dict(e)
             if item.published_date != data.published_date:
                 return item
             return False
         return None
     
-    async def update(self, data :Notice_B):
-        if data.id is None:
+    async def update(self, data :Notice):
+        if data._id is None:
             raise ValueError("The data must have an id to update")
         db = self.client["inha_notice"]
         collection = db["notice"]
 
-        result = await collection.update_one({"_id": data.id}, {"$set": data.to_dict()})
+        result = await collection.update_one({"_id": data._id}, {"$set": data.to_dict()})
         return result.modified_count > 0
 
     async def get_notices_by_filter(self, skip:int, limit:int, filters = {}) -> list:
@@ -52,6 +63,8 @@ class MongoDBWrapper:
         collection = db["notice"]
         
         filt = {}
+        if "id" in filters:
+            filt["_id"] = ObjectId(filters["id"])
         if "title" in filters:
             filt["title"] = {"$regex": filters["title"], "$options": "i"}
         if "content" in filters:
@@ -65,6 +78,10 @@ class MongoDBWrapper:
         cursor = collection.find(filt).skip(skip).limit(limit)
         return await cursor.to_list(length=limit)
     
+    def get_collection(self):
+        return self.client["inha_notice"]["notice"]
+
+
     async def count_notice(self, **kwargs) -> int:
         db = self.client["inha_notice"]
         collection = db["notice"]
@@ -80,6 +97,17 @@ class MongoDBWrapper:
             filt["source"] = kwargs["source"]
         
         return await collection.count_documents(filt)
+    
+    async def delete(self, id):
+        db = self.client["inha_notice"]
+        collection = db["notice"]
+
+        filt = {}
+        filt["_id"] = ObjectId(id)
+
+        result = await collection.delete_one(filt)
+        return result.deleted_count > 0
+
 
 if __name__ == "__main__":
     import asyncio
@@ -87,7 +115,7 @@ if __name__ == "__main__":
         db = await MongoDBWrapper.create()
         result = await db.get_notices_by_filter(skip = 0, limit = 20, filters={"source" : "학교공지"})
         for notice in result:
-            print(Notice_B.from_dict(notice))
+            print(Notice.from_dict(notice))
 
     asyncio.run(test())
         
