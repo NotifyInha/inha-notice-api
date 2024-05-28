f2c8dcc5d7698b8ea00da6acc0e4629dd77331b7
Myoung Hun <myeonghun1212@gmail.com>
2024-05-28 11:42:21 +0900

    11

--- MongoDBWrapper.py
+++ MongoDBWrapper.py
@@ -1,15 +1,16 @@
 import motor.motor_asyncio
-from DataModel import Notice
+from pymongo.server_api import ServerApi
+from DataModel import Notice_B
 from Config import connection_string
 
 class MongoDBWrapper:
     def __init__(self, client) -> None:
-        self.client = client
+        self.client:motor.motor_asyncio.AsyncIOMotorClient = client
 
     @classmethod
     async def create(cls):
         uri = connection_string
-        client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        client = motor.motor_asyncio.AsyncIOMotorClient(uri, server_api=ServerApi('1'))
         try:
             await client.admin.command("ping")
             print("Pinged your deployment. You successfully connected to MongoDB!")
@@ -17,7 +18,34 @@ class MongoDBWrapper:
         except Exception as e:
             print(type(e), e)
        
+    def insert(self, data :Notice_B):
+            # Get the database
+            db = self.client["inha_notice"]
+            # Get the collection
+            collection = db["notice"]
+            # Insert the data
+            if not self._check_duplicate(data):
+                collection.insert_one(data.to_dict())
+        
+    def need_update(self, data :Notice_B):
+        db = self.client["inha_notice"]
+        collection = db["notice"]
+        e = collection.find_one({"url": data.url}) 
+        if e is not None:
+            item = Notice_B.from_dict(e)
+            if item.published_date != data.published_date:
+                return item
+            return False
+        return None
+    
+    async def update(self, data :Notice_B):
+        if data.id is None:
+            raise ValueError("The data must have an id to update")
+        db = self.client["inha_notice"]
+        collection = db["notice"]
 
+        result = await collection.update_one({"_id": data.id}, {"$set": data.to_dict()})
+        return result.modified_count > 0
 
     async def get_notices_by_filter(self, skip:int, limit:int, filters = {}) -> list:
         db = self.client["inha_notice"]
@@ -57,9 +85,9 @@ if __name__ == "__main__":
     import asyncio
     async def test():
         db = await MongoDBWrapper.create()
-        result = await db.get_notices_by_filter(skip = 0, limit = 20, source = "학교공지")
+        result = await db.get_notices_by_filter(skip = 0, limit = 20, filters={"source" : "학교공지"})
         for notice in result:
-            print(Notice.from_dict(notice))
+            print(Notice_B.from_dict(notice))
 
     asyncio.run(test())
         
--- V1.py
+++ V1.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Optional
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, HTTPException, Query
 from fastapi.responses import JSONResponse
-from DataModel import Notice
+from DataModel import Notice, Notice_B
 from MongoDBWrapper import MongoDBWrapper
 
 router = APIRouter()
@@ -29,7 +29,7 @@ async def get_notices(skip : int = Query(0, ge = 0), limit : int = Query(20, ge 
     #데이터는 있는 상황
     total_count = await db.count_notice()
     f = ['id','title', 'url', 'category', 'source', 'published_date', 'scraped_date']
-    res = [Notice.from_dict(i).to_dict_with_fields(*f) for i in raw_res]
+    res = [Notice_B.from_dict(i).to_dict_with_fields(*f) for i in raw_res]
     respose = {
         "total_count" : total_count,
         "page" : skip // limit + 1,
@@ -45,4 +45,21 @@ async def get_notice(notice_id: str):
     f = ['id','title', 'content','url', 'category', 'source', 'published_date', 'scraped_date']
     temp = Notice.from_dict(raw_res[0])
     return temp.to_dict_with_fields(*f)
-    
+
+
+@router.post("/notices")
+async def post_notice(notice: Notice):
+    await db.insert_notice(notice)
+    return {"message" : "데이터가 성공적으로 저장되었습니다."}
+
+@router.put("/notices/{notice_id}")
+async def put_notice(notice_id: str, notice: Notice, force:bool = False):
+    notice.id = notice_id
+    before = await db.get_notices_by_filter(0,1,{"id" : notice.id})
+    if len(before) == 0:
+        raise HTTPException(status_code=404, detail="해당 id에 대한 데이터가 없습니다.")
+    before = Notice_B.from_dict(before[0])
+    if not force and await db.need_update(notice) is False:
+        raise HTTPException(status_code=409, detail="데이터가 이미 최신입니다.")
+    await db.update(notice)
+    return {"message" : "데이터가 성공적으로 수정되었습니다.", "before" : before.to_dict(), "after" : notice.to_dict()}
