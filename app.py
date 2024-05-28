c38fad13622cffe0b575d88fde67d0034d3e8214
Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
2024-05-29 00:24:14 +0900

    update requirements.txt

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ from V1 import router as v1_router
 app = FastAPI()
 app.include_router(v1_router, prefix="/api/v1")
 
+@app.get("/api")
+async def ping():
+    return {"message": "pong"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
