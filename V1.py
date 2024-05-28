0d81fde2bb78ef17f1c87f31bd07be8471df4dd0
Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
2024-05-28 16:13:36 +0900

    CRUD 모두 구현

--- V1.py
+++ V1.py
@@ -1,47 +1,67 @@
-import asyncio
-from typing import Optional
 from fastapi import APIRouter, HTTPException, Query
-from fastapi.responses import JSONResponse
-from DataModel import Notice, Notice_B
+from DataModel import Notice, NoticeLight
 from MongoDBWrapper import MongoDBWrapper
+from fastapi_pagination import Page, add_pagination
+from typing import Any, Dict, Optional
+
+from fastapi_pagination.api import apply_items_transformer, create_page
+from fastapi_pagination.bases import AbstractParams
+from fastapi_pagination.types import AdditionalData, AsyncItemsTransformer
+from fastapi_pagination.utils import verify_params
 
 router = APIRouter()
+
+async def paginate(
+    collection,
+    query_filter: Optional[Dict[Any, Any]] = None,
+    projection: Optional[Dict[Any, Any]] = None,
+    params: Optional[AbstractParams] = None,
+    sort: Optional[Any] = None,
+    *,
+    transformer: Optional[AsyncItemsTransformer] = None,
+    additional_data: Optional[AdditionalData] = None,
+    **kwargs: Any,
+) -> Any:
+    params, raw_params = verify_params(params, "limit-offset")
+    query_filter = query_filter or {}
+
+    total = await collection.count_documents(query_filter) if raw_params.include_total else None
+    cursor = collection.find(query_filter, projection, skip=raw_params.offset, limit=raw_params.limit, **kwargs)
+    if sort is not None:
+        cursor = cursor.sort(*sort) if isinstance(sort, tuple) else cursor.sort(sort)
+
+    items = await cursor.to_list(length=raw_params.limit)
+    t_items = await apply_items_transformer(items, transformer, async_=True)
+
+    return create_page(
+        t_items,
+        total=total,
+        params=params,
+        **(additional_data or {}),
+    )
+
+
 @router.on_event("startup")
 async def startup_event():
     global db
     db = await MongoDBWrapper.create()
 
 @router.get("/notices")
-async def get_notices(skip : int = Query(0, ge = 0), limit : int = Query(20, ge = 1), keyword : Optional[str] = None, category : Optional[str] = None, source : Optional[str] = None):
+async def get_notices( keyword : Optional[str] = Query(None), category : Optional[str] = Query(None), source : Optional[str] = Query(None)) ->Page[NoticeLight]:
     filt = {}
     if keyword is not None:
-        filt["title"] = keyword
-        filt["content"] = keyword
+        filt["$or"] = [{"title": {"$regex": keyword}}, {"content": {"$regex": keyword}}]
     if category is not None:
         filt["category"] = category
     if source is not None:
         filt["source"] = source
-
-    raw_res = await db.get_notices_by_filter(skip, limit, filters=filt)
-    if len(raw_res) == 0:
-        return JSONResponse(content = {"message" : "필터에 일치하는 데이터가 없습니다."}, status_code = 404)
-    
-    #데이터는 있는 상황
-    total_count = await db.count_notice()
-    f = ['id','title', 'url', 'category', 'source', 'published_date', 'scraped_date']
-    res = [Notice_B.from_dict(i).to_dict_with_fields(*f) for i in raw_res]
-    respose = {
-        "total_count" : total_count,
-        "page" : skip // limit + 1,
-        "page_size" : limit,
-        "total_page" : (total_count - 1) // limit + 1,
-        "notices" : res,
-    }
-    return respose
+    return await paginate(db.get_collection(), query_filter=filt, projection={"content": 0, "images": 0, "attached": 0, "is_sent_notification": 0, "scraped_date": 0})
 
 @router.get("/notices/{notice_id}")
 async def get_notice(notice_id: str):
-    raw_res = await db.get_notices_by_filter(skip = 0, limit= 1,id = notice_id)
+    raw_res = await db.get_notices_by_filter(skip = 0, limit= 1,filters={"id" : notice_id})
+    if len(raw_res) == 0:
+        raise HTTPException(status_code=404, detail="해당 id에 대한 데이터가 없습니다.")
     f = ['id','title', 'content','url', 'category', 'source', 'published_date', 'scraped_date']
     temp = Notice.from_dict(raw_res[0])
     return temp.to_dict_with_fields(*f)
@@ -49,17 +69,32 @@ async def get_notice(notice_id: str):
 
 @router.post("/notices")
 async def post_notice(notice: Notice):
-    await db.insert_notice(notice)
+    res = await db.insert(notice)
+    if not res:
+        raise HTTPException(status_code=409, detail="중복된 데이터가 존재합니다.")
     return {"message" : "데이터가 성공적으로 저장되었습니다."}
 
 @router.put("/notices/{notice_id}")
 async def put_notice(notice_id: str, notice: Notice, force:bool = False):
-    notice.id = notice_id
-    before = await db.get_notices_by_filter(0,1,{"id" : notice.id})
+    notice._id = notice_id
+    before = await db.get_notices_by_filter(0,1,{"id" : notice._id})
     if len(before) == 0:
         raise HTTPException(status_code=404, detail="해당 id에 대한 데이터가 없습니다.")
-    before = Notice_B.from_dict(before[0])
+    before = Notice.from_dict(before[0])
     if not force and await db.need_update(notice) is False:
         raise HTTPException(status_code=409, detail="데이터가 이미 최신입니다.")
     await db.update(notice)
     return {"message" : "데이터가 성공적으로 수정되었습니다.", "before" : before.to_dict(), "after" : notice.to_dict()}
+
+@router.delete("/notices/{notice_id}")
+async def delete_notice(notice_id: str):
+    before = await db.get_notices_by_filter(0,1,{"id" : notice_id})
+    if len(before) == 0:
+        raise HTTPException(status_code=404, detail="해당 id에 대한 데이터가 없습니다.")
+    before = Notice.from_dict(before[0])
+    res = await db.delete(notice_id)
+    if not res:
+        raise HTTPException(status_code=500)
+    return {"message" : "데이터가 성공적으로 삭제되었습니다.", "before" : before.to_dict()}
+
+add_pagination(router)
