a05ab10b3dacf03093cc6c9f06625fbbd604cfbc
Myoung Hun <myeonghun1212@gmail.com>
2024-05-30 10:46:35 +0900

    chore: Remove commented-out code in DataModel.py

--- app/DataModel.py
+++ app/DataModel.py
@@ -38,11 +38,6 @@ class NoticeCreate(BaseModel):
     attached:Optional[list] = []
     is_sent_notification:Optional[bool] = False
 
-    # def __init__(self, title, content, images, attached, url, category, source, published_date, scraped_date = None, is_sent_notification = False):
-    #     if scraped_date is None:
-    #         scraped_date = datetime.now().astimezone(local_timezone).isoformat()
-    #     super().__init__(title=title, content=content, images=images, attached=attached, url=url, category=category, source=source, published_date=published_date, scraped_date=scraped_date, is_sent_notification=is_sent_notification)
-
 class Notice(BaseModel):
     title:str
     url:str
@@ -55,45 +50,6 @@ class Notice(BaseModel):
     images:list
     attached:list
     is_sent_notification:bool
-
-    # def __init__(self, title, content, images, attached, url, category, source, published_date, scraped_date = None, is_sent_notification = False, _id = None):
-    #     if scraped_date is None:
-    #         scraped_date = datetime.now().astimezone(local_timezone).isoformat()
-    #     super().__init__(title=title, content=content, images=images, attached=attached, url=url, category=category, source=source, published_date=published_date, scraped_date=scraped_date, is_sent_notification=is_sent_notification, _id=_id, id=str(_id))
-
-    # def to_dict(self):
-    #     return {
-    #         "title": self.title,
-    #         "content": self.content,
-    #         "images": self.image,
-    #         "attached": self.attached,
-    #         "url": self.url,
-    #         "category": self.category,
-    #         "source": self.source,
-    #         "published_date": self.published_date,
-    #         "scraped_date": self.scraped_date,
-    #         "is_sent_notification": self.is_sent_notification
-    #     }
-    
-    # @classmethod
-    # def from_dict(cls, data: dict):
-    #     if "_id" in data:#_id가 있으면 id로 저장
-    #         id = data["_id"]
-    #     else:
-    #         id = None
-    #     title = data["title"]
-    #     content = data["content"]
-    #     image = data["images"]
-    #     attached = data["attached"]
-    #     url = data["url"]
-    #     category = data["category"]
-    #     source = data["source"]
-    #     published_date = data["published_date"]
-    #     scraped_date = data["scraped_date"]
-    #     is_sent_notification = data["is_sent_notification"]
-    #     return cls(title, content, image, attached, url, category, source, published_date, 
-    #                scraped_date = scraped_date, is_sent_notification = is_sent_notification, _id = id)
-
 
     def __str__(self):
         return f"title: {self.title}, content: {self.content}, image: {self.image}, attached: {self.attached}, url: {self.url}, category: {self.category}, source: {self.source}, published_date: {self.published_date}, scraped_date: {self.scraped_date}, is_sent_notification: {self.is_sent_notification}"
