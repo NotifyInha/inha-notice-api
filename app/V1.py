20234c7aefcf1b1a8ef5313c483dabbfbd0c4e49
Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
2024-06-03 17:52:53 +0900

    Squashed commit of the following:
    commit 369c1c36837ca90b3de9ca151b7f1c15ee3e868a
    Author: Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
    Date:   Mon Jun 3 17:51:28 2024 +0900
    
        프론트랑 명세 맞추기
    
    commit 75ebd9d570caf131bc692cb9429f99db92e45515
    Author: Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
    Date:   Fri May 31 01:02:55 2024 +0900
    
        source filtering
    
    commit 623ed928b75983c3dcebd89f29111affe44cd251
    Author: Myoung Hun <myeonghun1212@gmail.com>
    Date:   Thu May 30 11:39:31 2024 +0900
    
        todo : convert string to isodate
    
    commit 65696e0a7edfa48b1026cfd2b86a2b6f72f60896
    Author: Myoung Hun <myeonghun1212@gmail.com>
    Date:   Thu May 30 10:47:23 2024 +0900
    
        Remove unused Page class in DataModel.py
    
    commit a05ab10b3dacf03093cc6c9f06625fbbd604cfbc
    Author: Myoung Hun <myeonghun1212@gmail.com>
    Date:   Thu May 30 10:46:35 2024 +0900
    
        chore: Remove commented-out code in DataModel.py
    
    commit 35080fb34cd4e8bb48c722ce6f3f0a2c88798b5b
    Author: Myoung Hun <myeonghun1212@gmail.com>
    Date:   Thu May 30 10:41:53 2024 +0900
    
        pydantic 수정
    
    commit b0f501ab566f7f9bcc3db623f55f5589b9fdaede
    Author: Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
    Date:   Thu May 30 02:08:02 2024 +0900
    
        chore: Update launch.json for Python Debugger configuration
    
    commit aebea05d5a7d3ed3e522f718e83996992cae15f5
    Author: Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
    Date:   Wed May 29 22:39:30 2024 +0900
    
        chore: Remove unused import statement in MongoDBWrapper.py

--- app/V1.py
+++ app/V1.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from fastapi import APIRouter, HTTPException, Query
-from DataModel import Notice, NoticeLight
+import pytz
+from DataModel import Notice, NoticeCreate, NoticeGet, NoticeUpdate
 from MongoDBWrapper import MongoDBWrapper
 from fastapi_pagination import Page, add_pagination
 from typing import Any, Dict, Optional
@@ -9,7 +11,12 @@ from fastapi_pagination.bases import AbstractParams
 from fastapi_pagination.types import AdditionalData, AsyncItemsTransformer
 from fastapi_pagination.utils import verify_params
 
+from Univutil.UnivFactory import InhaUnivFactory
+
 router = APIRouter()
+local_timezone = pytz.timezone('Asia/Seoul')
+#대학 변경시 변경
+univFunction = InhaUnivFactory().get_univutil()
 
 async def paginate(
     collection,
@@ -46,58 +53,65 @@ async def startup_event():
     global db
     db = await MongoDBWrapper.create()
 
-@router.get("/notices")
-async def get_notices( keyword : Optional[str] = Query(None), category : Optional[str] = Query(None), source : Optional[str] = Query(None), url : Optional[str] = Query(None)) ->Page[NoticeLight]:
-    filt = {}
+@router.get("/notices", description="필터링된 공지사항 목록을 반환합니다.")
+async def get_notices( keyword : Optional[str] = Query(None), category : Optional[str] = Query(None), sourcefilter : Optional[int] = Query(0), major : Optional[str] = Query(None) ,date:Optional[str] = Query(None),url : Optional[str] = Query(None)) ->Page[NoticeGet]:
+    sourcefilter = univFunction.deserializesource(sourcefilter)
+    filt = {"$and" : []}
     if keyword is not None:
-        filt["$or"] = [{"title": {"$regex": keyword}}, {"content": {"$regex": keyword}}]
+        filt["$and"].append({"$or" : [{"title": {"$regex": keyword}}, {"content": {"$regex": keyword}}]})
     if category is not None:
-        filt["category"] = category
-    if source is not None:
-        filt["source"] = source
+        filt["$and"].append({"category":category})
+    if sourcefilter != []:
+        filt["$and"].append({"$or" : [{"source": i} for i in sourcefilter]})
     if url is not None:
-        filt["url"] = url
+        filt["$and"].append({"url":url})
+    if date is not None:
+        dates = date.split(',')
+        filt["$and"].append({"published_date" : {"$gte" : datetime.fromisoformat(dates[0]), "$lte" : datetime.fromisoformat(dates[1])}})
+    if filt["$and"] == []:
+        filt = {}
+    return await paginate(db.get_collection(), query_filter=filt, sort={"published_date" : -1}, projection={"content": 0, "images": 0, "attached": 0, "is_sent_notification": 0, "scraped_date": 0})
 
-    return await paginate(db.get_collection(), query_filter=filt, projection={"content": 0, "images": 0, "attached": 0, "is_sent_notification": 0, "scraped_date": 0})
-
-@router.get("/notices/{notice_id}")
-async def get_notice(notice_id: str):
+@router.get("/notices/{notice_id}" , description="특정 id에 대한 공지사항을 반환합니다.")
+async def get_notice(notice_id: str) -> Notice:
     raw_res = await db.get_notices_by_filter(skip = 0, limit= 1,filters={"id" : notice_id})
     if len(raw_res) == 0:
         raise HTTPException(status_code=404, detail="해당 id에 대한 데이터가 없습니다.")
-    f = ['id','title', 'content','url', 'category', 'source', 'published_date', 'scraped_date']
-    temp = Notice.from_dict(raw_res[0])
-    return temp.to_dict_with_fields(*f)
+    f = ('_id','title', 'content','url', 'category', 'source', 'published_date', 'scraped_date', 'images', 'attached')
+    temp = Notice.model_validate(raw_res[0])
+    return temp
 
 
-@router.post("/notices")
-async def post_notice(notice: Notice):
+@router.post("/notices", description="새로운 공지사항을 저장합니다. 기존 공지사항을 업데이트 하기 위해선 PUT를 사용해야합니다.")
+async def post_notice(notice: NoticeCreate):
+    notice = notice.model_dump()
+    notice['scraped_date'] = datetime.now().astimezone(local_timezone).isoformat()
     res = await db.insert(notice)
     if not res:
         raise HTTPException(status_code=409, detail="중복된 데이터가 존재합니다.")
     return {"message" : "데이터가 성공적으로 저장되었습니다."}
 
-@router.put("/notices/{notice_id}")
-async def put_notice(notice_id: str, notice: Notice, force:Optional[bool] = False):
+@router.put("/notices/{notice_id}" , description="특정 id에 대한 공지사항을 수정합니다. 제공되지 않은 필드는 수정되지 않습니다. force가 True일 경우 데이터가 최신이 아니더라도 수정합니다.")
+async def put_notice(notice_id: str, notice: NoticeUpdate, force:Optional[bool] = False):
     notice._id = notice_id
-    before = await db.get_notices_by_filter(0,1,{"id" : notice._id})
+    before = await db.get_notices_by_filter(0,1,{"id" : notice_id})
     if len(before) == 0:
         raise HTTPException(status_code=404, detail="해당 id에 대한 데이터가 없습니다.")
-    before = Notice.from_dict(before[0])
-    if not force and await db.need_update(notice) is False:
+    before = Notice.model_validate(before[0])
+    if not force and await db.need_update(notice.model_dump()) is False:
         raise HTTPException(status_code=409, detail="데이터가 이미 최신입니다.")
-    await db.update(notice)
-    return {"message" : "데이터가 성공적으로 수정되었습니다.", "before" : before.to_dict(), "after" : notice.to_dict()}
+    await db.update(notice_id, notice)
+    return {"message" : "데이터가 성공적으로 수정되었습니다.", "before" : before.model_dump(), "after" : notice.model_dump()}
 
-@router.delete("/notices/{notice_id}")
+@router.delete("/notices/{notice_id}", description="특정 id에 대한 공지사항을 삭제 후 삭제한 공지사항을 반환합니다.")
 async def delete_notice(notice_id: str):
     before = await db.get_notices_by_filter(0,1,{"id" : notice_id})
     if len(before) == 0:
         raise HTTPException(status_code=404, detail="해당 id에 대한 데이터가 없습니다.")
-    before = Notice.from_dict(before[0])
+    before = Notice.model_validate(before[0])
     res = await db.delete(notice_id)
     if not res:
         raise HTTPException(status_code=500)
-    return {"message" : "데이터가 성공적으로 삭제되었습니다.", "before" : before.to_dict()}
+    return {"message" : "데이터가 성공적으로 삭제되었습니다.", "before" : before.model_dump()}
 
 add_pagination(router)
--- app/main.py
+++ app/main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 import uvicorn
 from V1 import router as v1_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Notify Inha",
+    description="인하알리미의 openapi 문서입니다",
+    version="0.1.0",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc"
+)
 app.include_router(v1_router, prefix="/api/v1")
+
 
 @app.get("/api")
 async def ping():
